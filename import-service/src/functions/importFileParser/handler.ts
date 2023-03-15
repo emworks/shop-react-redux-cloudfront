100cf9231dc0e7cf2377baa807f9e0992368b20a
Yevgeny Yakushev <yevgeny_yakushev@epam.com>
2023-03-15 21:08:46 +0400

    move to parsed

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -2,35 +2,42 @@ import type { ValidatedEventS3Event } from '@libs/s3';
 import AWS from 'aws-sdk';
 
 import csv from 'csv-parser'
+import { s3BucketKey, s3BucketParsedKey } from 'src/constants';
 
 const importFileParser: ValidatedEventS3Event = async (event) => {
   console.log('event', event)
 
   const s3 = new AWS.S3()
 
-  const promises = event.Records.map((record) => {
+  for (const record of event.Records) {
     const Bucket = record.s3.bucket.name;
     const Key = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
     const params = { Bucket, Key };
     const stream = s3.getObject(params).createReadStream();
 
-    return new Promise(function (resolve, reject) {
-      stream.pipe(csv())
-        .on('data', (data) => {
-          console.log(data);
-        })
-        .on('error', (error) => {
-          console.error(error);
-          reject(error);
-        })
-        .on('end', (rows) => {
-          console.log(`Parsed ${rows} rows`);
-          resolve(rows);
-        });
-    });
-  });
+    const parser = stream.pipe(csv())
 
-  return Promise.all(promises);
+    try {
+      for await (const data of parser) {
+        console.log(data);
+
+        await s3.copyObject({
+          Bucket,
+          CopySource: `${Bucket}/${record.s3.object.key}`,
+          Key: record.s3.object.key.replace(s3BucketKey, s3BucketParsedKey)
+        }).promise()
+
+        await s3.deleteObject({
+          Bucket,
+          Key: record.s3.object.key
+        }).promise()
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    console.log(`Parsed`);
+  }
 };
 
 export const main = importFileParser
--- import-service/src/libs/s3.ts
+++ import-service/src/libs/s3.ts
@@ -1,1 +1,3 @@
-export type ValidatedEventS3Event = any
+import type { S3Handler } from "aws-lambda/trigger/s3"
+
+export type ValidatedEventS3Event = S3Handler
