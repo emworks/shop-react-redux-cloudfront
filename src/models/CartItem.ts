990df35f35c953a5c80c8c4880e00d588e63b4a9
Yevgeny Yakushev <yevgeny_yakushev@epam.com>
2023-11-21 23:54:14 +0400

    implemented cart service

--- src/models/CartItem.ts
+++ src/models/CartItem.ts
@@ -4,3 +4,15 @@ export type CartItem = {
   product: Product;
   count: number;
 };
+
+export type CartResponseItem = {
+  id: string;
+  count: number;
+};
+
+export type CartResponse = {
+  data: {
+    id: string;
+    items: CartResponseItem[];
+  };
+};
--- src/queries/cart.ts
+++ src/queries/cart.ts
@@ -2,16 +2,36 @@ import axios, { AxiosError } from "axios";
 import React from "react";
 import { useQuery, useQueryClient, useMutation } from "react-query";
 import API_PATHS from "~/constants/apiPaths";
-import { CartItem } from "~/models/CartItem";
+import { CartItem, CartResponse } from "~/models/CartItem";
+import { AvailableProduct } from "~/models/Product";
 
 export function useCart() {
   return useQuery<CartItem[], AxiosError>("cart", async () => {
-    const res = await axios.get<CartItem[]>(`${API_PATHS.cart}/profile/cart`, {
-      headers: {
-        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-      },
+    const cartRes = await axios.get<CartResponse>(
+      `${API_PATHS.cart}/profile/cart`,
+      {
+        headers: {
+          Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+        },
+      }
+    );
+
+    const productRes = await axios.get<AvailableProduct[]>(
+      `${API_PATHS.bff}/products`
+    );
+
+    return cartRes.data.data.items.map(({ id, count }) => {
+      const product = productRes.data.find((product) => product.id == id);
+      return {
+        product: {
+          id,
+          title: product?.title ?? id,
+          description: product?.description ?? id,
+          price: product?.price ?? Number(id),
+        },
+        count,
+      };
     });
-    return res.data;
   });
 }
 
