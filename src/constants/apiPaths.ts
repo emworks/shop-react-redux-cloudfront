8549bcf605c58f68900c02186f27470cdf8cc989
Yevgeny Yakushev <yevgeny_yakushev@epam.com>
2023-11-21 12:42:43 +0400

    update cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://jo936llnz4.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pncu28d3e3.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://jo936llnz4.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://m0ivxkhf99.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://m0ivxkhf99.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
